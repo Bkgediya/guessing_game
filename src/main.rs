3fe04e7f8166228a0627bd4f9e7632b5dca9c9d6
Bhavdip Gediya <bhavdipgediya099@gmail.com>
2025-12-14 20:35:28 +0530

    guessing game project

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    println!("Guess the number");
+    let secret_number= rand::thread_rng().gen_range(1..=100);
+    loop {
+        println!("Please input your number");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed t  o read line");
+
+        let guess: u32  = match  guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue 
+        };
+
+        println!("You guess {}",guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Win");
+                break;
+        }
+    }
+
+    println!("You guess {}",guess)
+}
+
+}
